02075150ca8c7550c4e922b8c5ae7fa537dd6569
JulesBecquelin <jules.becquelin@edu.univ-fcomte.fr>
2025-03-11 10:31:36 +0100

    backend + testBack fini

--- src/js/backend.mjs
+++ src/js/backend.mjs
@@ -2,12 +2,12 @@ import PocketBase from 'pocketbase' ;
 const pb = new PocketBase('http://127.0.0.1:8090') ;
 
 export async function allFilms() {
-    const a= await pb.collection('Films').getFullList({ filter :'+Date_de_projection'}) ;
+    const a= await pb.collection('Films').getFullList({ sort :'+Date_de_projection'}) ;
     return a
 };
 
 export async function allActivite(){
-    const b = await pb.collection('Activites').getFullList({ sort :'+date'});
+    const b = await pb.collection('Activites').getFullList({ sort :'+Date'});
     return b
 };
 
@@ -17,7 +17,66 @@ export async function allInvite() {
     
 };
 
-export async function oneID(id) {
+export async function FilmById(id) {
     const d = await pb.collection('Films').getOne(id) ;
     return d
+};
+
+export async function ActiviteById(id) {
+    const e = await pb.collection('Activites').getOne(id) ;
+    return e
 }
+
+export async function InviteById(id) {
+    const e = await pb.collection('Invites').getOne(id) ;
+    return e
+};
+
+export async function allActiviteByInviteId(id) {
+    const f = await pb.collection('Activites').getFullList( {filter : `Invite.id='${id}'`, expand : 'Invites' });
+    return f;
+};
+
+export async function allActiviteByInviteName(Nom) {
+    const f = await pb.collection('Activites').getFullList( {filter : `Invite.Nom='${Nom}'`, expand : 'Invites' });
+    return f;
+};
+
+export async function addNewActivite(newActivite){
+
+    await pb.collection('Activites').create(newActivite);
+};
+
+export async function addNewInvite(newInvite){
+
+    await pb.collection('Invites').create(newInvite);
+};
+
+export async function addNewFilm(newFilm){
+
+    await pb.collection('Films').create(newFilm);
+};
+
+export async function updateActiviteById(id){
+    await pb.collection('Activites').update(id);
+};
+
+export async function updateInviteById(id){
+    await pb.collection('Invites').update(id);
+};
+
+export async function updateFilmById(id){
+    await pb.collection('Films').update(id);
+};
+
+export async function deleteActiviteById(id){
+    await pb.collection('Activites').delete(id);
+};
+
+export async function deleteInviteById(id){
+    await pb.collection('Invites').delete(id);
+};
+
+export async function deleteFilmById(id){
+    await pb.collection('Films').delete(id);
+};
---
+++ testback.js
@@ -0,0 +1,153 @@
+import {allFilms,allActivite,allInvite,FilmById,ActiviteById,InviteById,allActiviteByInviteId,allActiviteByInviteName,addNewActivite,addNewInvite,addNewFilm,updateActiviteById,updateInviteById,updateFilmById,deleteActiviteById,deleteInviteById,deleteFilmById} from "./src/js/backend.mjs";
+
+
+// try{
+//     const a = await allFilms();
+//     console.log(JSON.stringify(a,null, 2)) ;
+//     } catch (e) {
+//     console.error(e) ;
+//     }
+
+// try{
+    // const a = await allActivite();
+    // console.log(JSON.stringify(a,null, 2)) ;
+    // } catch (e) {
+    // console.error(e) ;
+    // }
+// 
+
+// try{
+//     const a = await allInvite();
+//     console.log(JSON.stringify(a,null, 2)) ;
+//     } catch (e) {
+//     console.error(e) ;
+//     }
+
+// try{
+//     const a = await FilmById("69178v1w79e5a21");
+//     console.log(JSON.stringify(a,null, 2)) ;
+//     } catch (e) {
+//     console.error(e) ;
+//     }
+
+// try{
+//     const a = await ActiviteById("2d76tfs3y97r57q");
+//     console.log(JSON.stringify(a,null, 2)) ;
+//     } catch (e) {
+//     console.error(e) ;
+//     }
+
+
+// try{
+//     const a = await InviteById("jlkryd4r1sg78ud");
+//     console.log(JSON.stringify(a,null, 2)) ;
+//     } catch (e) {
+//     console.error(e) ;
+//     }
+
+// try{
+//     const a = await allActiviteByInviteId("7dymk6t51ip9lfz");
+//     console.log(JSON.stringify(a,null, 2)) ;
+//     } catch (e) {
+//     console.error(e) ;
+//     }
+
+// try{
+//     const a = await allActiviteByInviteName("Amélie Mauresmo");
+//     console.log
+//     (JSON.stringify(a,null, 2)) ;
+//     } catch (e) {
+//     console.error(e) ;
+//     }
+
+// try{
+//     const a = {
+//         "Titre": "ToToLand",
+//         "Organisateur": "Jean LaSalle",
+//         "Description": "c'est bi1",
+//         "Date": "2030-05-02 23:00:00.123Z",
+//         "Invite": "3ees408c0i3ejuj"
+//     };
+//     await addNewActivite(a);
+// }catch(e) {
+//     console.error(e);
+// }
+
+// try{
+//     const a = {
+//         "Biographie": "C'est un mec bi1",
+//         "Nom": "Jean LaSalle",
+//         "Activite": "3f107sv5y422hm6",
+//     };
+//     await addNewInvite(a);
+// }catch(e) {
+//     console.error(e);
+// }
+
+// try{
+//     const a = {
+//     "Titre": "La vie d'un mec bi1",
+//     "Synopsis" : "c'est l'histoite d'un mec bi1",
+//     "BA": "https://UnMecBi1LeFilm.fr",
+//     "Genre" : "Documentaire",
+//     "Date_de-projection" : "2030-05-02 23:00:00.123Z",
+//     };
+//     await addNewFilm(a);
+// }catch(e) {
+//     console.error(e);
+// }
+
+// try{
+//     const a = {
+//         "Titre": "TataLand",
+//         "Organisateur": "Jean TOTO",
+//         "Description": "c'est pas un mec bi1",
+//         "Date": "2030-05-02 23:00:00.123Z",
+//         "Invite": "3ees408c0i3ejuj"
+//     };
+//     await updateActiviteById("we1ar7d8f7u5hpe",a);
+// }catch(e) {
+//     console.error(e);
+// }
+
+// try{
+//     const a = {
+//         "Biographie": "C'est pas un mec bi1",
+//         "Nom": "Jean LaSalle",
+//         "Activite": "3f107sv5y422hm6",
+//     };
+//     await updateInviteById("2dr27un8i5af65s",a);
+// }catch(e) {
+//     console.error(e);
+// }
+
+// try{
+//     const a = {
+//     "Titre": "La vie d'un mec pas bi1",
+//     "Synopsis" : "c'est l'histoite d'un mec pas bi1",
+//     "BA": "https://UnMecPasBi1LeFilm.fr",
+//     "Genre" : "Film",
+//     "Date_de-projection" : "2030-04-02 23:00:00.123Z",
+//     };
+//     await updateFilmById("v6q0d6oy96tlhss",a);
+// }catch(e) {
+//     console.error(e);
+// }
+
+// try{
+//     await deleteFilmById("v6q0d6oy96tlhss");
+// }catch(e){
+//     console.error(e);
+// }
+
+// try{
+//     await deleteActiviteById("we1ar7d8f7u5hpe");
+// }catch(e){
+//     console.error(e);
+// }
+
+// try{
+//     await deleteInviteById("2dr27un8i5af65s");
+// }catch(e){
+//     console.error(e);
+// }
