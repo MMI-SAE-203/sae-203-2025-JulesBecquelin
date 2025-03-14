791fb69e31dfe717edb3774c2b5244ce36bae5cb
JulesBecquelin <jules.becquelin@edu.univ-fcomte.fr>
2025-03-14 10:43:11 +0100

    ajout netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,20 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+
   output: 'server',
+
   experimental: {
     svg : true,
   
-  }
+  },
+
+  adapter: netlify()
 });
